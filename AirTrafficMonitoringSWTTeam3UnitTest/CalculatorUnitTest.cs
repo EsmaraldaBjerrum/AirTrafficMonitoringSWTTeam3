4f6c823fbd80bb6e3ba7cb94c29f1ac1b2913642
Esmaralda Bjerrum <201708266@post.au.dk>
2019-03-10 10:36:36 +0100

    Ændring af testklasse class1 til CalculatorUnitTest og opsætning af dele af testklassen

---
+++ AirTrafficMonitoringSWTTeam3UnitTest/CalculatorUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using AirTrafficMonitoringSWTTeam3;
+
+namespace AirTrafficMonitoringSWTTeam3UnitTest
+{
+    [TestFixture]
+    public class CalculatorUnitTest
+    {
+        private Calculator uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void CalculateCompassCourse_()
+        {
+
+        }
+    }
+}
