872cbfe65bb8bf18e9448aec6352479314fd8c58
Esmaralda Bjerrum <201708266@post.au.dk>
2019-03-10 10:22:56 +0100

    Udregning af compassCourse

--- AirTrafficMonitoringSWTTeam3/Calculator.cs
+++ AirTrafficMonitoringSWTTeam3/Calculator.cs
@@ -35,8 +35,50 @@ namespace AirTrafficMonitoringSWTTeam3
                 {
                     if (aircraft.Tag == currentAircraft.Tag)
                     {
+                        double xDifference = currentAircraft.XCoordinate - aircraft.XCoordinate;
+                        double yDifference = currentAircraft.YCoordinate - aircraft.YCoordinate;
 
-                        // Kode til at udregne kompasretning
+                        if (xDifference == 0)
+                        {
+                            if (yDifference > 0)
+                            {
+                                aircraft.CompassCourse = 0;
+                            }
+                            else
+                            {
+                                aircraft.CompassCourse = 180;
+                            }
+                        }
+
+                        else if (yDifference == 0)
+                        {
+                            if (xDifference > 0)
+                            {
+                                aircraft.CompassCourse = 90;
+                            }
+                            else
+                            {
+                                aircraft.CompassCourse = 270;
+                            }
+                        }
+
+                        else
+                        {
+                            aircraft.CompassCourse = Convert.ToInt32(Math.Round(Math.Atan(Math.Abs(xDifference / yDifference))));
+
+                            if (xDifference > 0 && yDifference < 0)
+                            {
+                                aircraft.CompassCourse += 90;
+                            }
+                            if (xDifference < 0 && yDifference < 0)
+                            {
+                                aircraft.CompassCourse += 180;
+                            }
+                            if (xDifference < 0 && yDifference > 0)
+                            {
+                                aircraft.CompassCourse += 270;
+                            }
+                        }
                     }
                 }
             }
