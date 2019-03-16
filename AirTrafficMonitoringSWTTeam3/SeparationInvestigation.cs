16e51ca865ff94bbbf02ad163d689a8354ec84aa
Esmaralda Bjerrum <201708266@post.au.dk>
2019-03-16 15:35:35 +0100

    Oprettelse af separationWarning event, opsat separationskriterier

--- AirTrafficMonitoringSWTTeam3/SeparationInvestigation.cs
+++ AirTrafficMonitoringSWTTeam3/SeparationInvestigation.cs
@@ -19,12 +19,26 @@ namespace AirTrafficMonitoringSWTTeam3
 
         public void Separation(object sender, AirspaceDataEventArgs e)
         {
-            foreach (Aircraft aircarft in e.TransponderData)
+            foreach (Aircraft aircraft in e.TransponderData)
             {
-                for (int i = 0; i < e.TransponderData.Count; i++)
+                foreach (Aircraft comparingAircraft in e.TransponderData)
                 {
-                   
+                    if (aircraft.Tag != comparingAircraft.Tag)
+                    {
+                        int verticalSeparation = Math.Abs(aircraft.Altitude - comparingAircraft.Altitude);
+                        double xDifference = Math.Abs(aircraft.XCoordinate - comparingAircraft.XCoordinate);
+                        double yDifference = Math.Abs(aircraft.YCoordinate - comparingAircraft.YCoordinate);
+                        double horizontalSeparation = Math.Sqrt(Math.Pow(xDifference, 2) + Math.Pow(yDifference, 2));
+
+                        if (verticalSeparation < 300 && horizontalSeparation < 5000)
+                        {
+                            //Hvis separationen allerede er kaldt, så skal den ikke kaldes igen
+                            //Oprettelse af lokal liste, der husker hvem, der er under separation
+                        }
+                    }
+                       
                 }
+              
             }
 
             
