045e318e15aee8008573defdc3cebb679a30d211
Esmaralda Bjerrum <201708266@post.au.dk>
2019-03-21 15:36:29 +0100

    Ændringer i test

--- AirTrafficMonitoringSWTTeam3UnitTest/FormattingUnitTest.cs
+++ AirTrafficMonitoringSWTTeam3UnitTest/FormattingUnitTest.cs
@@ -4,87 +4,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AirTrafficMonitoringSWTTeam3;
+using AirTrafficMonitoringSWTTeam3.Controler;
+using AirTrafficMonitoringSWTTeam3.Events;
 using NSubstitute;
+using NSubstitute.Routing.Handlers;
 using NUnit.Framework;
 using TransponderReceiver;
 
 namespace AirTrafficMonitoringSWTTeam3UnitTest
 {
-   [TestFixture]
-  public class FormattingUnitTest
-   {
-      private LogToScreen _fakePrint;
-      private SeparationInvestigation _fakeSeparationInvestigation;
-      private Calculator _fakeCalculator;
-      private ITransponderReceiver _fakeTransponderReceiver;
-      private Formatting_Tracks uut;
-     
+    [TestFixture]
+    public class FormattingUnitTest
+    {
+        private LogToScreen _fakePrint;
+        private SeparationInvestigation _fakeSeparationInvestigation;
+        //private Calculator _fakeCalculator;
+        private IUpdater _fakeUpdater;
+        private ILog _fakeLog;
+        private ITransponderReceiver _fakeTransponderReceiver;
+        private Formatting_Tracks uut;
 
-      [SetUp]
-      public void SetUp()
-      {
-         _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
-         _fakeCalculator = new Calculator(_fakeTransponderReceiver);
-         _fakePrint = Substitute.For<LogToScreen>();
-         _fakeSeparationInvestigation = Substitute.For<SeparationInvestigation>(_fakeCalculator);
-         uut = new Formatting_Tracks();
-        
-      }
 
-      [Test]
-      public void StringToPrintTracksOnScreen()
-      {
-         List<Aircraft> fakeList = new List<Aircraft>();
-         Aircraft fakeAircraft = new Aircraft("SKF", 21, 8, 1996, DateTime.Now);
-         fakeAircraft.CompassCourse = 180;
-         fakeAircraft.HorizontalVelocity = 2019;
-         fakeList.Add(fakeAircraft);
-         
-       uut.StringToPrintTracksOnScreen(fakeList);
+        [SetUp]
+        public void SetUp()
+        {
+            _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
+            //_fakeCalculator = new Calculator(_fakeTransponderReceiver);
+            _fakeUpdater = Substitute.For<IUpdater>();
+            _fakeLog = Substitute.For<ILog>();
+            _fakePrint = Substitute.For<LogToScreen>();
+            _fakeSeparationInvestigation = Substitute.For<SeparationInvestigation>(_fakeUpdater);
+            uut = new Formatting_Tracks(_fakeUpdater, _fakeLog);
 
-         _fakePrint.Received().Log("Tag: SKF Current position: X: 21 meters, Y: 8 meters, Current altitude: 1996 meters, Current horizontal velocity: 2019 m/s, Current compass course 180 degress");
-       
-      }
+        }
 
-      [Test]
-      public void SeparationOccur_Eventfired_StringReadyToFile()
-      {
-         List<SeparationWarningData> fakeList = new List<SeparationWarningData>();
-         SeparationWarningData fakeSeparationWarningData = new SeparationWarningData("SKF", "LBS", (DateTime.ParseExact("20191203213426980", "yyyyMMddHHmmssfff",
-             System.Globalization.CultureInfo.InvariantCulture)));
-         //fakeSeparationWarningData.AircraftTag1 = "SKF";
-         //fakeSeparationWarningData.AircraftTag2 = "LBS";
-         //fakeSeparationWarningData.SeparationTimeStamp = (DateTime.ParseExact("20191203213426980", "yyyyMMddHHmmssfff",
-           // System.Globalization.CultureInfo.InvariantCulture));
+        [Test]
+        public void StringToPrintTracksOnScreen()
+        {
+            List<Aircraft> fakeList = new List<Aircraft>();
+            Aircraft fakeAircraft = new Aircraft("SKF", 21, 8, 1996, DateTime.Now);
+            fakeAircraft.CompassCourse = 180;
+            fakeAircraft.HorizontalVelocity = 2019;
+            fakeList.Add(fakeAircraft);
 
-         fakeList.Add(fakeSeparationWarningData);
+            _fakeUpdater.UpdatedDataEvent += Raise.EventWith(this, new UpdatedDataEvent(fakeList));
+            
+            
 
-         _fakeSeparationInvestigation.SeparationWarningDataEvent +=
-            Raise.EventWith(new SeparationWarningDataEvent(fakeList));
+            _fakePrint.Received().Log("Tag: SKF Current position: X: 21 meters, Y: 8 meters, Current altitude: 1996 meters, Current horizontal velocity: 2019 m/s, Current compass course 180 degress");
 
-         _fakePrint.Received().Log("RunSeparationInvestigation condition between SKF and LBS at 03-12-2019 21:34:26");
+        }
 
-      }
+        [Test]
+        public void SeparationOccur_Eventfired_StringReadyToFile()
+        {
+            List<SeparationWarningData> fakeList = new List<SeparationWarningData>();
+            SeparationWarningData fakeSeparationWarningData = new SeparationWarningData("SKF", "LBS", (DateTime.ParseExact("20191203213426980", "yyyyMMddHHmmssfff",
+                System.Globalization.CultureInfo.InvariantCulture)));
+            //fakeSeparationWarningData.AircraftTag1 = "SKF";
+            //fakeSeparationWarningData.AircraftTag2 = "LBS";
+            //fakeSeparationWarningData.SeparationTimeStamp = (DateTime.ParseExact("20191203213426980", "yyyyMMddHHmmssfff",
+            // System.Globalization.CultureInfo.InvariantCulture));
 
-    [Test]
-      public void SeparationOccur_Eventfired_StringReadyToScreen()
-      {
-         List<SeparationWarningData> fakeList = new List<SeparationWarningData>();
-         SeparationWarningData fakeSeparationWarningData = new SeparationWarningData("SKF", "LBS", DateTime.ParseExact("20191203213426980", "yyyyMMddHHmmssfff",
-             System.Globalization.CultureInfo.InvariantCulture));
-         //fakeSeparationWarningData.AircraftTag1 = "SKF";
-         //fakeSeparationWarningData.AircraftTag2 = "LBS";
-         //fakeSeparationWarningData.SeparationTimeStamp = (DateTime.ParseExact("20191203213426980", "yyyyMMddHHmmssfff",
-           // System.Globalization.CultureInfo.InvariantCulture));
+            fakeList.Add(fakeSeparationWarningData);
 
-         fakeList.Add(fakeSeparationWarningData);
+            _fakeSeparationInvestigation.SeparationWarningDataEvent +=
+               Raise.EventWith(new SeparationWarningDataEvent(fakeList));
 
-         _fakeSeparationInvestigation.SeparationWarningDataEvent +=
-            Raise.EventWith(new SeparationWarningDataEvent(fakeList));
+            _fakePrint.Received().Log("RunSeparationInvestigation condition between SKF and LBS at 03-12-2019 21:34:26");
 
-         _fakePrint.Received().Log("RunSeparationInvestigation condition between SKF and LBS at 03-12-2019 21:34:26");
+        }
 
-      }
+        [Test]
+        public void SeparationOccur_Eventfired_StringReadyToScreen()
+        {
+            List<SeparationWarningData> fakeList = new List<SeparationWarningData>();
+            SeparationWarningData fakeSeparationWarningData = new SeparationWarningData("SKF", "LBS", DateTime.ParseExact("20191203213426980", "yyyyMMddHHmmssfff",
+                System.Globalization.CultureInfo.InvariantCulture));
+            //fakeSeparationWarningData.AircraftTag1 = "SKF";
+            //fakeSeparationWarningData.AircraftTag2 = "LBS";
+            //fakeSeparationWarningData.SeparationTimeStamp = (DateTime.ParseExact("20191203213426980", "yyyyMMddHHmmssfff",
+            // System.Globalization.CultureInfo.InvariantCulture));
 
-   }
+            fakeList.Add(fakeSeparationWarningData);
+
+            _fakeSeparationInvestigation.SeparationWarningDataEvent +=
+               Raise.EventWith(new SeparationWarningDataEvent(fakeList));
+
+            _fakePrint.Received().Log("RunSeparationInvestigation condition between SKF and LBS at 03-12-2019 21:34:26");
+
+        }
+
+    }
 }
