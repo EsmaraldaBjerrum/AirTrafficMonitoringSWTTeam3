5df0904075426a7174bb618cc10b012e5197ee52
Esmaralda Bjerrum <201708266@post.au.dk>
2019-03-24 19:01:05 +0100

    Oprettelse af DTO-mappe

---
+++ AirTrafficMonitoringSWTTeam3/DTO/SeparationWarningData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTrafficMonitoringSWTTeam3
+{
+    public class SeparationWarningData
+    {
+        public string AircraftTag1 { get; set; }
+        public string AircraftTag2 { get; set; }
+        public DateTime SeparationTimeStamp { get; set; }
+
+        public SeparationWarningData(string aircraftTag1, string aircraftTag2, DateTime separationTimeStamp)
+        {
+            AircraftTag1 = aircraftTag1;
+            AircraftTag2 = aircraftTag2;
+            SeparationTimeStamp = separationTimeStamp;
+        }
+    }
+}
--- AirTrafficMonitoringSWTTeam3/Formatting_Tracks.cs
+++ AirTrafficMonitoringSWTTeam3/Formatting_Tracks.cs
@@ -10,29 +10,29 @@ using TransponderReceiver;
 
 namespace AirTrafficMonitoringSWTTeam3
 {
-   public class Formatting_Tracks
-   {
-      private ILog _log;
-       private IUpdater _updater;
-     
+    public class Formatting_Tracks
+    {
+        private ILog _log;
+        private IUpdater _updater;
 
-      public Formatting_Tracks(IUpdater updater, ILog log)
-      {
-          _log = log;
-          _updater = updater;
-          _updater.UpdatedDataEvent += StringToPrintTracksOnScreen;
 
-      }
+        public Formatting_Tracks(IUpdater updater, ILog log)
+        {
+            _log = log;
+            _updater = updater;
+            _updater.UpdatedDataEvent += StringToPrintTracksOnScreen;
 
-      public void StringToPrintTracksOnScreen(object sender, UpdatedDataEvent e)
-      {
-         foreach (Aircraft track in e.UpdatedData)
-         {
-           string trackToPrint = "Tag: "+track.Tag + " Current position: X: " + track.XCoordinate + " meters, Y: " + track.YCoordinate + " meters, Current altitude: " + track.Altitude + " meters, Current horizontal velocity: " + track.HorizontalVelocity + " m/s, Current compass course " + track.CompassCourse + " degress. " ;
-            _log.Log(trackToPrint);
-         }
-      }
+        }
 
-     
-   }
+        public void StringToPrintTracksOnScreen(object sender, UpdatedDataEvent e)
+        {
+            foreach (Aircraft track in e.UpdatedData)
+            {
+                string trackToPrint = "Tag: " + track.Tag + " Current position: X: " + track.XCoordinate + " meters, Y: " + track.YCoordinate + " meters, Current altitude: " + track.Altitude + " meters, Current horizontal velocity: " + track.HorizontalVelocity + " m/s, Current compass course " + track.CompassCourse + " degress. ";
+                _log.Log(trackToPrint);
+            }
+        }
+
+
+    }
 }
