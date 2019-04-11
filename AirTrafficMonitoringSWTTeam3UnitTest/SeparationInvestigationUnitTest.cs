9c9796cabf52cbc4f5406d83f8265ed2d38ecf31
Louise <Louis@LAPTOP-ONA38G07>
2019-04-11 11:21:21 +0200

    Ændring af SeparationInvestigationUnitTest

--- AirTrafficMonitoringSWTTeam3UnitTest/SeparationInvestigationUnitTest.cs
+++ AirTrafficMonitoringSWTTeam3UnitTest/SeparationInvestigationUnitTest.cs
@@ -16,41 +16,25 @@ namespace AirTrafficMonitoringSWTTeam3UnitTest
     {
         private SeparationInvestigation _uut;
         private IUpdater fakeUpdater;
-        private ILog fakeLogToLog;
-        private ILog fakeLogToScreen;
-        private Formatting_Separation fakeFormattingSeparation;
+      
         private SeparationWarningDataEvent _event;
 
         [SetUp]
         public void SetUp()
         {
             fakeUpdater = Substitute.For<IUpdater>();
-            fakeLogToLog = Substitute.For<ILog>();
-            fakeLogToScreen = Substitute.For<ILog>();
+            
             _uut = new SeparationInvestigation(fakeUpdater);
-            fakeFormattingSeparation = Substitute.For<Formatting_Separation>(_uut, fakeLogToLog, fakeLogToScreen);
+           
 
             _event = null;
             _uut.SeparationWarningDataEvent += (o, args) => { _event = args; };
         }
 
+        
 
         [Test]
-        [TestCase("AAA111", 10000, 10000, 1000, "CAA111", 50000, 40000, 5000, 0)]
-        [TestCase("BAA111", 10000, 51000, 1000, "DAA111", 10000, 50000, 1000, 1)]
-        public void AddSeparation_AircraftXAndYIsISeparation_ZNewSeparationIsAdded(string t1, int x1, int y1, int a1, string t2, int x2, int y2, int a2, int numberOfSeparations)
-        {
-            List<Aircraft> testAircraft = new List<Aircraft>();
-            testAircraft.Add(new Aircraft(t1, x1, y1, a1, DateTime.Now));
-            testAircraft.Add(new Aircraft(t2, x2, y2, a2, DateTime.Now));
-
-            _uut.AddSeparations(testAircraft);
-
-            Assert.That(_uut.newSeparationWarningData.Count.Equals(numberOfSeparations));
-        }
-
-        [Test]
-        [TestCase("1AA111", 40000, 51000, 2000, "BAA111", 10000, 5000, 1000, "FAA111", 30000, 10000, 5000, 0)]
+     
         [TestCase("BAA111", 10000, 51000, 1000, "DAA111", 10000, 50000, 1000, "EAA111", 30000, 40000, 5000, 1)]
         [TestCase("BAA111", 10000, 51000, 1200, "DAA111", 10000, 50000, 1000, "EAA111", 10000, 49000, 800, 2)]
         [TestCase("BAA111", 10000, 50000, 1000, "DAA111", 10000, 50000, 1000, "EAA111", 10000, 50000, 1000, 3)]
@@ -62,14 +46,30 @@ namespace AirTrafficMonitoringSWTTeam3UnitTest
             testAircraft.Add(new Aircraft(t2, x2, y2, a2, DateTime.Now));
             testAircraft.Add(new Aircraft(t3, x3, y3, a3, DateTime.Now));
 
-            _uut.AddSeparations(testAircraft);
+            fakeUpdater.UpdatedDataEvent += Raise.EventWith(this, new UpdatedDataEvent(testAircraft));
 
-            Assert.That(_uut.newSeparationWarningData.Count, Is.EqualTo(numberOfSeparations));
+            Assert.That(_event.TransponderData.Count, Is.EqualTo(numberOfSeparations));
         }
 
         [Test]
-        [TestCase("1AA111", 40000, 51000, 2000, "BAA111", 10000, 5000, 1000, "FAA111", 30000, 10000, 5000, "BAA111", 10000, 51000, 1000, "DAA111", 10000, 50000, 1000, "EAA111", 30000, 40000, 5000, 2)]
-        [TestCase("BAA111", 10000, 51000, 1000, "DAA111", 10000, 50000, 1000, "EAA111", 30000, 40000, 5000, "1AA111", 40000, 51000, 2000, "BAA111", 10000, 5000, 1000, "FAA111", 30000, 10000, 5000, 2)]
+        [TestCase("1AA111", 40000, 51000, 2000, "BAA111", 10000, 5000, 1000, "FAA111", 30000, 10000, 5000, 0)]
+        
+        public void AddSeparation_IsNull(string t1, int x1, int y1,
+            int a1, string t2, int x2, int y2, int a2, string t3, int x3, int y3, int a3, int numberOfSeparations)
+        {
+            List<Aircraft> testAircraft = new List<Aircraft>();
+            testAircraft.Add(new Aircraft(t1, x1, y1, a1, DateTime.Now));
+            testAircraft.Add(new Aircraft(t2, x2, y2, a2, DateTime.Now));
+            testAircraft.Add(new Aircraft(t3, x3, y3, a3, DateTime.Now));
+
+            fakeUpdater.UpdatedDataEvent += Raise.EventWith(this, new UpdatedDataEvent(testAircraft));
+
+            Assert.That(_event, Is.Null);
+        }
+
+        [Test]
+        [TestCase("1AA111", 40000, 51000, 2000, "BAA111", 10000, 5000, 1000, "FAA111", 30000, 10000, 5000, "BAA111", 10000, 51000, 1000, "DAA111", 10000, 50000, 1000, "EAA111", 30000, 40000, 5000, 1)]
+        [TestCase("1AA111", 40000, 51000, 2000, "BAA111", 10000, 5000, 1000, "FAA111", 30000, 10000, 5000, "BAA111", 10000, 51000, 1200, "DAA111", 10000, 50000, 1000, "EAA111", 10000, 49000, 800, 2)]
         [TestCase("BAA111", 10000, 51000, 1200, "DAA111", 10000, 50000, 1000, "EAA111", 10000, 49000, 800,  "BAA111", 10000, 51000, 1000, "DAA111", 10000, 50000, 1000, "EAA111", 30000, 40000, 5000, 0)]
         
         public void AddSeparation_AircraftXAndYAndQIsInSeparation_ZNewSeparationsIsAdded(string t1, int x1, int y1,
@@ -92,31 +92,11 @@ namespace AirTrafficMonitoringSWTTeam3UnitTest
 
             
 
-            Assert.That(_event.newSeparationWarningData.Count, Is.EqualTo(numberOfSeparations));
+            Assert.That(_event.TransponderData.Count, Is.EqualTo(numberOfSeparations));
         }
 
 
-        [Test]
-        [TestCase("AAA111", "BBBB111", "CCC111", "DDD111", "EEE111", "FFF111", "GGG111", "HHH111", 2)]
-        [TestCase("AAA111", "BBBB111", "GGG111", "HHH111", "EEE111", "FFF111", "GGG111", "HHH111", 1)]
-        [TestCase("EEE111", "FFF111", "GGG111", "HHH111", "EEE111", "FFF111", "GGG111", "HHH111", 0)]
-
-        public void SeparationController_XOldAndYNew_ZNew(string a, string b, string c, string d, string e, string f, string g, string h, int numberOfNewSeparations)
-        {
-            List<SeparationWarningData> testOld = new List<SeparationWarningData>();
-            List<SeparationWarningData> testNew = new List<SeparationWarningData>();
-
-            testOld.Add(new SeparationWarningData(a, b, DateTime.Now));
-            testOld.Add(new SeparationWarningData(c, d, DateTime.Now));
-
-            testNew.Add(new SeparationWarningData(e, f, DateTime.Now));
-            testNew.Add(new SeparationWarningData(g, h, DateTime.Now));
-            
-
-            _uut.SeparationController(testOld,testNew);
-
-            Assert.That(_uut.newSeparationWarningData.Count, Is.EqualTo(numberOfNewSeparations));
-        }
+       
 
         [Test]
         public void SeparationInvestigation_Calls_FormattingSeparationToFileMethode()
@@ -140,32 +120,10 @@ namespace AirTrafficMonitoringSWTTeam3UnitTest
 
             Assert.That(_event.TransponderData, Is.Not.Null);
 
-            //fakeFormattingSeparation.Received(1).StringToPrintSeparationInFile(this, new SeparationWarningDataEvent(_uut.newSeparationWarningData));
+            
 
         }
-        //[Test]
-        //public void SeparationInvestigation_Calls_FormattingSeparationToScreenMethode()
-        //{
-        //    List<Aircraft> testData = new List<Aircraft>();
-        //    testData.Add(new Aircraft("ATR423", 85045, 12932, 14000, DateTime.ParseExact("20151006213456789",
-        //        "yyyyMMddHHmmssfff",
-        //        System.Globalization.CultureInfo.InvariantCulture)));
-        //    testData.Add(new Aircraft("BCD123", 85045, 12932, 14000, DateTime.ParseExact("20151006213456789",
-        //        "yyyyMMddHHmmssfff",
-        //        System.Globalization.CultureInfo.InvariantCulture)));
-        //    testData.Add(new Aircraft("XYZ98", 85000, 75654, 4000, DateTime.ParseExact("20151006213456789",
-        //        "yyyyMMddHHmmssfff",
-        //        System.Globalization.CultureInfo.InvariantCulture)));
-        //    testData.Add(new Aircraft("XYZ986", 90059, 90654, 4000, DateTime.ParseExact("20151006213456789",
-        //        "yyyyMMddHHmmssfff",
-        //        System.Globalization.CultureInfo.InvariantCulture)));
-
-        //    // Act: Trigger the fake object to execute event invocation
-        //   _uut.RunSeparationInvestigation(this, new UpdatedDataEvent(testData));
-
-        //    fakeFormattingSeparation.Received(1).StringToPrintSeparationToScreen(this, new SeparationWarningDataEvent(_uut.newSeparationWarningData));
-
-        //}
+      
 
     }
 }
